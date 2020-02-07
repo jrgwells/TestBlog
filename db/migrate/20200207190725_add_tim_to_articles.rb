32817bbdf1f3a919401ed293ddf1db2a91329b2c
Jack Wells <contact@jackwells.dev>
2020-02-07 19:43:40 +0000

    Created articles table, model and added basic validations

---
+++ db/migrate/20200207190725_add_tim_to_articles.rb
@@ -0,0 +1,7 @@
+class AddTimToArticles < ActiveRecord::Migration[5.1]
+  def change
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
