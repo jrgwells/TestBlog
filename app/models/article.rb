392c29d527d639e9cd5d023c5ac891c6e88ff690
Jack Wells <contact@jackwells.dev>
2020-02-10 19:43:39 +0000

    Add edit and update function (and partial)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   belongs_to :user
-  before_save { self.email = email.downcase }
+  # before_save { self.email = email.downcase }
   # This stops articles with nil values, and sets minimum and maximum length.
   validates :title, presence: true, length: { minimum: 3, maximum: 100 } 
   validates :description, presence: true, length: { minimum: 10, maximum: 300} 
