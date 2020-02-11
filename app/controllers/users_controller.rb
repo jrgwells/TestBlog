29d2d2d163c3535ff284f223928eedee5fb5f90f
Jack Wells <contact@jackwells.dev>
2020-02-11 13:40:43 +0000

    fix signup logout bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to TestBlog, #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -36,7 +37,6 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 
   end
-
 
   private
   def user_params
