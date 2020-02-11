942b3585a9da5a58f906e384e73db7be079e0671
Jack Wells <contact@jackwells.dev>
2020-02-11 14:34:11 +0000

    Add admin priv

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :articles
+  has_many :articles, dependent: :destroy
   validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 60 }, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
