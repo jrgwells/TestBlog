8a10c06407968e593cc32c51c58b765bad8cb608
Jack Wells <contact@jackwells.dev>
2020-02-11 01:11:16 +0000

    Added greeting, UI fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+  def greeting
+    if Time.now > "00:00".to_time && Time.now < "11:59".to_time
+      return "Morning" 
+    elsif Time.now > "11:59".to_time && Time.now < "17:59".to_time
+      return "Afternoon"
+    elsif Time.now > "18:00".to_time && Time.now < "23:59".to_time
+      return "Evening"
+    end
+  end
+  
   def gravatar_for(user, options = { size: 80})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
