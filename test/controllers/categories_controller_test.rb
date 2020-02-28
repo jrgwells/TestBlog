82064a9ac8bef92302f46bf35b3da0f076cf005f
Jack Wells <contact@jackwells.dev>
2020-02-28 20:28:09 +0000

    admin restriction

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -26,7 +26,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_no_difference 'Category.count' do
       post categories_path, params: { category: { name: "sports" } }
     end
-    assert_redirected_to 'categories/path'
+    assert_redirected_to categories_path
   end
 
 end
