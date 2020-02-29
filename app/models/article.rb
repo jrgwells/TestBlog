ba65d60b44c2dbc70fadc720eca82762f986c121
Jack Wells <jack@Studio.localdomain>
2020-02-29 16:16:31 +0000

    syntax fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   has_many :article_categories
-  has many :articles, through: :article_categories
+  has_many :articles, through: :article_categories
   # before_save { self.email = email.downcase }
   # This stops articles with nil values, and sets minimum and maximum length.
   validates :title, presence: true, length: { minimum: 3, maximum: 100 } 
